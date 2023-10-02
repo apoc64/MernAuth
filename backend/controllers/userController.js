73796c54b822a09a634917304d3ec802e77e57f3
Steven Schwedt <sschwedt@gmail.com>
2023-10-02 16:03:32 -0600

    User can login and logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,25 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc   Auth user/set token
 // route   POST /api/users/login
 // access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'Auth User'
-  })
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid email or password')
+  }
 })
 
 // @desc   Register New User
@@ -30,6 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
+    generateToken(res, user._id)
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -45,8 +58,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // route   POST /api/users/logout
 // access  Public
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+
   res.status(200).json({
-    message: 'Logout User'
+    message: 'User logged out'
   })
 })
 
