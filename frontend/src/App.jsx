f34ee45dba73d3e27d3798eb8e4d843da1318db0
Steven Schwedt <sschwedt@gmail.com>
2023-10-03 11:31:31 -0600

    Sets up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <HomeScreen />
+    </>
+  )
+}
+
+export default App
