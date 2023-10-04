6277504c99f72e0415680d1f8946e89b53ecf704
Steven Schwedt <sschwedt@gmail.com>
2023-10-04 11:39:37 -0600

    Adds cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -11,6 +12,9 @@ const port = process.env.PORT || 8000;
 connectDB();
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:4000'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
